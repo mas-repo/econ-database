54b3902cc4ea60a83685db7b4ee944626ccfb363
Mas <mas1110@gmail.com>
2026-02-06 14:58:46 +0800

    Update generate_index.py

--- PastPaper/generate_index.py
+++ PastPaper/generate_index.py
@@ -4,7 +4,7 @@ import sys
 
 # Configuration
 IGNORE_DIRS = {'.git', '__pycache__', '.github', 'js', 'css', 'img', 'projects'}
-IGNORE_FILES = {'index.html', 'generate_index.py', 'protect.js'}
+IGNORE_FILES = {'index.html', 'generate_index.py', 'protect.js', 'sync_database.py'}
 
 # HTML Template
 HTML_TEMPLATE = """
